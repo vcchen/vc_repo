ce270dc3948aa9cb1b9b2e7145ab586606a4e2b1
Vince Chen <cjh347@163.com>
2012-11-02 19:00:19 +0800

    updated at today

---
+++ zhuangxiu.mm
@@ -0,0 +1,13 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1351850726093" ID="ID_766629763" MODIFIED="1351850733781" TEXT="&#x88c5;&#x4fee;">
+<node CREATED="1351850734546" ID="ID_967660586" MODIFIED="1351850739218" POSITION="right" TEXT="&#x5ba2;&#x5385;">
+<node CREATED="1351850770155" ID="ID_1334682236" MODIFIED="1351850785733" TEXT="&#x63d2;&#x5ea7;">
+<node CREATED="1351850787202" ID="ID_323926958" MODIFIED="1351850819873" TEXT="&#x6839;&#x636e;&#x7535;&#x89c6;&#x67dc;&#x9ad8;&#x5ea6;&#x786e;&#x5b9a;&#x4f4d;&#x7f6e;"/>
+<node CREATED="1351850821264" ID="ID_1605472742" MODIFIED="1351850863013" TEXT="&#x5f3a;&#x5f31;&#x7535;&#x63a5;&#x53e3;&#x8981;&#x76f8;&#x5dee;20&#x516c;&#x5206;&#x4ee5;&#x4e0a;"/>
+</node>
+</node>
+<node CREATED="1351850739827" ID="ID_1432708916" MODIFIED="1351850761702" POSITION="right" TEXT="&#x53a8;&#x623f;"/>
+<node CREATED="1351850763140" ID="ID_1368573196" MODIFIED="1351850767546" POSITION="right" TEXT="&#x536b;&#x751f;&#x95f4;"/>
+</node>
+</map>
